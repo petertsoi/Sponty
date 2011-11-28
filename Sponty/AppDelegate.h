daef985a10e3c414cf5dcda307b27ff93bf1f111
Peter Tsoi <petertsoi@gmail.com>
2011-11-27 23:13:21 -0800

    Initial Commit

---
+++ Sponty/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Sponty
+//
+//  Created by Peter Tsoi on 11/27/11.
+//  Copyright (c) 2011 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
