3cf27a17f510490c9bc0750cef8d9bef34d0c747
Peter Tsoi <petertsoi@berkeley.edu>
2012-02-03 01:12:51 -0800

    Changed buttons, get directions now works.

--- PlaceMapView.h
+++ PlaceMapView.h
@@ -9,17 +9,31 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 #import <QuartzCore/QuartzCore.h>
+#import <CoreLocation/CoreLocation.h>
 
 @class PlaceViewController;
 
-@interface PlaceMapView : UIView <MKMapViewDelegate> {
+@interface PlaceMapView : UIView <MKMapViewDelegate, CLLocationManagerDelegate> {
     PlaceViewController * mDelegate;
     IBOutlet MKMapView * mMapView;
+    
+    CLLocationCoordinate2D placeLocation;
+    CLLocationManager * locationManager;
+    CLLocationCoordinate2D myLocation;
 }
 
 @property (nonatomic, retain) PlaceViewController * delegate;
+@property (nonatomic, retain) CLLocationManager *locationManager;  
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation;
+
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error;
 
 - (void) roundCorners;
 - (void) setLatLong:(CLLocationCoordinate2D)loc;
+- (IBAction)getDirections:(id)sender;
 
 @end
--- PlaceMapView.m
+++ PlaceMapView.m
@@ -13,6 +13,25 @@
 @implementation PlaceMapView
 
 @synthesize delegate = mDelegate;
+@synthesize locationManager;
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation {
+    myLocation = newLocation.coordinate;
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error
+{
+	NSLog(@"Error: %@", [error description]);
+}
+
+- (void) awakeFromNib {
+    self.locationManager = [[[CLLocationManager alloc] init] autorelease];
+    self.locationManager.delegate = self; // send loc updates to myself
+    [locationManager startUpdatingLocation];
+}
 
 - (void) setLatLong:(CLLocationCoordinate2D)loc {
     //pacific st. 36.97177,-122.026584&fspn=0.00984,0.013497
@@ -22,6 +41,8 @@
     [mMapView setRegion:region animated:YES];
     
     MKPlacemark * dest = [[MKPlacemark alloc] initWithCoordinate:loc addressDictionary:nil];
+    
+    placeLocation = loc;
     
     [mMapView addAnnotation:dest];
 
@@ -36,6 +57,10 @@
     [mDelegate hideMapView];
 }
 
+- (IBAction) getDirections:(id)sender {
+    [[UIApplication sharedApplication] openURL:[[NSURL alloc] initWithString:[NSString stringWithFormat:@"http://maps.google.com/maps?daddr=%f,%f&saddr=%f,%f", placeLocation.latitude, placeLocation.longitude, myLocation.latitude, myLocation.longitude]]];
+}
+
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation {
     MKPinAnnotationView *pinView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
     if(pinView == nil) {
@@ -46,6 +71,11 @@
         pinView.annotation = annotation;
     }
     return pinView;
+}
+
+- (void) dealloc {
+    [locationManager release];
+    [super dealloc];
 }
                                   
                                   
